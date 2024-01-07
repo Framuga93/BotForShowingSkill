21b5135b7029af3c0a11cda6d1b76ed7124a08e5
aleksejdemockin <alekseydemochkin93@gmail.com>
2024-01-07 19:51:02 +0300

    create project -add maven-spring-boot

---
+++ src/main/java/com/github/Framuga93/telegrambot/TelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.Framuga93.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotApplication.class, args);
+	}
+
+}
